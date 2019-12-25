f3cabe36aac97ce01e56235fdc58153f2b45be83
Felipe <felipeaugusto1291@gmail.com>
2019-12-25 14:23:34 -0300

    Add 1-9 and 1-10

---
+++ ch1/1-9.rkt
@@ -0,0 +1,39 @@
+; (+ 4 5)
+
+; Using the first procedure rewrite below:
+
+; (define (+ a b)
+;	(if (= a 0) b (inc (+ (dec a) b))))
+
+; By the substitution model:
+
+; (+ 4 5)
+; (inc (+ 3 5))
+; (inc (inc (+ 2 5)))
+; (inc (inc (inc (+ 1 5))))
+; (inc (inc (inc (inc (+ 0 5)))))
+; (inc (inc (inc (inc 5))))
+; (inc (inc (inc 6)))
+; (inc (inc 7))
+; (inc 8)
+; 9
+
+; The process above evolves in a recursive manner.
+
+; Using the second procedure rewrite below:
+
+; (define (+ a b)
+;	(if (= a 0) b (+ (dec a) (inc b))))
+
+; By the substitution model:
+
+; (+ 4 5)
+; (+ 3 6)
+; (+ 2 7)
+; (+ 1 8)
+; (+ 0 9)
+; 9
+
+; The process above evolves in a iterative manner.
+
+
